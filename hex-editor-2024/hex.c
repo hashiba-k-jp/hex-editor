e304513a86042b87f845a3ed98a91d698bcd16c6
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2024-08-06 23:53:54 +0900

    update

--- hex-editor-2024/hex.c
+++ hex-editor-2024/hex.c
@@ -5,107 +5,143 @@
 */
 
 
-// #include <termios.h>
-// #include <stdlib.h>
 #include <stdio.h>
-// #include <stdint.h>
-// #include <errno.h>
-// #include <string.h>
-// #include <ctype.h>
-// #include <sys/ioctl.h>
-#include <unistd.h>  // getopt()
-// #include <getopt.h>
-#include <err.h>    // warn
-
-
-
 #include <stdbool.h>
 
+// #include <stdint.h>
+// #include <string.h>
+// #include <ctype.h>
+// #include <getopt.h>
 
+#include <unistd.h>  // getopt(),
+#include <err.h>    // warn(), err(),
+#include <errno.h>  // errno,
+#include <sys/ioctl.h>  // struct insize,
+#include <termios.h>    // struct termios,
+#include <stdlib.h> // atexit(),
+#include <string.h> // memcmp(),
+// #include <ncurses.h>    // ncurses:initscr(),
+
+
+#include "config.h"
+#include "terminalMode.h"
+#include "keys.h"
+
+int keyProcess(int c, char* msg, EDITOR EDITOR);
+int keyInput();
 
 int main(int argc, char *argv[]){
 
-    bool test_color = false;
-    char* input_file = NULL; // This should be stored in the other var.
-    // https://www.mm2d.net/main/prog/c/getopt-02.html
-    // refer -> man 3 getopt
+    struct termios COOKED_MODE;
+    EDITOR EDITOR = init_editor();
+
     int ch;
-
-
     while ((ch = getopt(argc, argv, "i:cm")) != -1) {
         switch (ch) {
             case 'c':
-                test_color = true;
+                EDITOR.isColored = true;
                 break;
             case 'i':
-                if(input_file == NULL){
-                    input_file = optarg;
+                if(EDITOR.filename == NULL){
+                    EDITOR.filename = optarg;
                 }else{
-                    warn("[WARN]too many input files; run \"%s -m\"", argv[0]);
+                    warn("[WARN]too many input files; run \"%s -m\" to read manual.", argv[0]);
                 }
                 break;
             case 'm':
-                // usage();
+                print_usage();
                 return 0;
         }
     }
 
-    printf("%s\n", input_file);
+    atexit(fin_program);
+    save_cooked_mode();
+    switch_raw_mode();
 
+    // printf("[DEBUG] FILENAME : %s\n", EDITOR.filename);
+    // printf("[DEBUG] ROW : %d\n", EDITOR.ws.ws_row);
+    // printf("[DEBUG] COL : %d\n", EDITOR.ws.ws_col);
 
-     //         switch (ch) {
-     //         case 'b':
-     //                 bflag = 1;
-     //                 break;
-     //         case 'f':
-     //                 if ((fd = open(optarg, O_RDONLY, 0)) < 0) {
-     //                         (void)fprintf(stderr,
-     //                             "myname: %s: %s\n", optarg, strerror(errno));
-     //                         exit(1);
-     //                 }
-     //                 break;
-     //         case '?':
-     //         default:
-     //                 usage();
-     //         }
-     // }
+    char *msg;
+    int input_c;
+    int res_process;
+
+    while(1){
+        input_c = keyInput();
+        res_process = keyProcess(input_c, msg, EDITOR);
+        // if(input_c == CTRL_Q){
+        //     return 0;
+        // }else{
+        //     printf(" -> %c\r\n", input_c);
+        // }
+    }
+
+    return 0;
+}
+
+int keyProcess(int c, char* msg, EDITOR EDITOR){
+    switch (c) {
+        case CTRL_Q:
+            // [Todo] Save or not.
+            if(EDITOR.isEdited){
+
+            }else{
+                exit(EXIT_SUCCESS);
+            }
+    }
+    return 1;
+}
+
+int keyInput(){
+    // input : key input
+    // output : key number defined in keys.h (NOT ascii code)
+
+    int read_len; // s means Status
+    char c, seq[4];
+
+    while((read_len = read(STDIN_FILENO, &seq, 4)) == 0){
+        // pass; This while waits any input.
+    }
+    if(read_len == -1){
+        // failed to get input from key.
+        exit(3);
+    }
+    // printf("[[%d %x %x %x %x]]", read_len, seq[0], seq[1], seq[2], seq[3]);
+
+    if(read_len == 1){
+        if(0x20 <= seq[0] && seq[0] <= 0x7F){
+            return seq[0];
+        }else{
+            switch(seq[0]){
+                case TAB:
+                case CTRL_G:
+                case CTRL_Q:
+                case CTRL_S:
+                case CTRL_X:
+                case ESC:
+                    return seq[0];
+                    break;
+                default:
+                    return 0x00;
+            }
+        }
+    }else if(read_len == 2){
+        switch(seq[2]){
+            case 0x41:
+                return ARR_UP;
+                break;
+            case 0x42:
+                return ARR_DW;
+                break;
+            case 0x43:
+                return ARR_RI;
+                break;
+            case 0x44:
+                return ARR_LE;
+                break;
+        }
+    }
 
 
     return 0;
-
-
-/*
-  // initEditor();
-
-  // int pot, longindex
-
-  //getopt_long is standard c library
-  //getopt_long(int argc, char * const *argv, const char *optstring, const struct option *longopts, int *longindex);
-
-  // 色をつけるかどうかのコマンドライン引数？
-  // while((opt = getopt_long(argc, argv, "ac", longopts, &longindex)) != -1){
-  //   switch (opt) {
-  //       case 'a':
-  //           // E.dispascii = 1;
-  //           break;
-  //       case 'c':
-  //           E.iscolored = 1;
-  //           break;
-  //   }
-  // }
-
-  // currently, the input file is required.
-  // [todo] when run without any input, the program should make new file.
-  if(argc < 2){
-      fprintf(stderr,"Usage: ./hex <filename>\n");
-      exit(1);
-  }
-
-  editorOpen();
-
-  // [todo] when run without any input, the program should make new file.
-  readfile(argv[argc-1]);
-*/
-
-
 }
